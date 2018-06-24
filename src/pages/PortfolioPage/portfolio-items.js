db6b79a3bf0fa34e967b4293b91f04c2158d5e82
Diogo <diogomqbm13@gmail.com>
2018-06-24 02:40:32 -0300

    initial commit

---
+++ src/pages/PortfolioPage/portfolio-items.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+export default [{
+  name: 'typy',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        CookAll.
+      </div>
+      <div className='portfolio-item__desc'>
+        A mobile app to help you with your daily cooking life.
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-js"></i>
+        <i className="fab fa-react"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="http://cookall.launchrock.com/">More</a>
+      </div>
+    </div>
+  )
+}, {
+  name: 'dev-landing-page',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        This page
+      </div>
+      <div className='portfolio-item__desc'>
+        My personal dev landing page
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-html5"></i>
+        <i className="fab fa-css3"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="https://dev.to/flexdinesh/create-your-developer-landing-page-with-github-pages---42jk">Blog</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/flexdinesh/dev-landing-page">More</a>
+      </div>
+    </div>
+  )
+}, {
+  name: 'react-redux-boilerplate',
+  render: (
+    <div className='portfolio-item'>
+      <div className='portfolio-item__title'>
+        Looking for more!
+      </div>
+      <div className='portfolio-item__desc'>
+        Want to add something here? Contact me!
+      </div>
+      <div className='portfolio-item__icon'>
+        <i className="fab fa-react"></i>
+        <i className="fab fa-js"></i>
+      </div>
+      <div className='portfolio-item__links'>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/diogomqbm">Linkedin</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://dev.to/diogomqbm_">Dev.to</a>
+      </div>
+    </div>
+  )
+}]
